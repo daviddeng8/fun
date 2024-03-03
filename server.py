cbd7d21609f48e0bd3188fefb263c1f4c46c6f08
David Deng <daviddeng8@gmail.com>
2024-03-03 01:36:08 -0800

    Moved everything to the localhost:8000 route

--- server.py
+++ server.py
@@ -5,26 +5,16 @@ from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 
+API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
+API_KEY = os.environ["API_KEY"]
+headers = {"Authorization": f"Bearer {API_KEY}"}
 
 async def homepage(request):
-    return JSONResponse({'hello': 'world'})
+	payload = {"inputs": "When did Brady win his first chip?"}
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return JSONResponse(response.json()[0])
 
 
 app = Starlette(debug=True, routes=[
     Route('/', homepage),
 ])
-
-API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
-API_KEY = os.environ["API_KEY"]
-headers = {"Authorization": f"Bearer {API_KEY}"}
-
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-	
-output = query({
-	"inputs": "When did Brady win his first chip?",
-})
-
-
-print(output)
